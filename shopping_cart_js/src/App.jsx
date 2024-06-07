473b593d21dce97254e2cc41b167dfe3b0aa4d0c
Kedar Dhoble <kedardhoble2001@gmail.com>
2024-06-07 11:28:50 +0530

    created the basic template of the shopping cart.

--- shopping_cart_js/src/App.jsx
+++ shopping_cart_js/src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react"
-
+import { Navbar } from "./layout/header/Navbar";
+import ContainerRouter from "./navigation/ContainerRouter";
+import { useState } from "react";
 function App() {
+  const [quantity, setQuantity] = useState(0);
 
   return (
-    <>
-  Hello
-    </>
-  )
+    <div className="App">
+      <Navbar quantity={quantity} />
+      <ContainerRouter quantity={quantity} setQuantity={setQuantity} />
+    </div>
+  );
 }
 
-export default App
+export default App;

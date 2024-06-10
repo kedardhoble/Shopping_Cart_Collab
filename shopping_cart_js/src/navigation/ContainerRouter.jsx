67d9e89b510fd6d9fec16989eabb1e27da7c2dba
Kedar Dhoble <kedardhoble2001@gmail.com>
2024-06-10 15:04:12 +0530

    Removed all the setQuantity and quantity defined using useState and const, and replaced them with the context made inside the redux folder.

--- shopping_cart_js/src/navigation/ContainerRouter.jsx
+++ shopping_cart_js/src/navigation/ContainerRouter.jsx
@@ -3,14 +3,14 @@ import Home from '../pages/home/Home'
 import Store from '../pages/store/Store'
 import About from '../pages/about/About'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-const ContainerRouter = ({quantity,setQuantity}) => {
+const ContainerRouter = () => {
   return (
     <div>
     <div className="mb-4 container">
         <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store  quantity={quantity} setQuantity={setQuantity}/>} />
+          <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
         </Routes>
         </BrowserRouter>

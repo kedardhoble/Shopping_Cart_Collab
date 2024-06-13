66d1c1deaee2bb0d81c86dc9af8472057d779440
Kedar Dhoble <kedardhoble2001@gmail.com>
2024-06-13 23:47:31 +0530

    Just for learning from scratch I started the new app without using Bootstrap.

---
+++ shopping_cart_new/shopping_cart_no_bs/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom"
+
+
+export function Navbar() {
+    return (
+        <>
+            <nav className="navbar-container">
+                <div className="logo-container">
+
+                </div>
+                <div className="nav-container">
+
+                </div>
+                <div className="cart-container"></div>
+            </nav>
+        </>
+    )
+}
